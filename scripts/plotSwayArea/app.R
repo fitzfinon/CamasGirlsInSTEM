e8adbecb3f5f65dd2f3a7b272d8d868227834774
Benjamin Chan <benjamin.ks.chan@gmail.com>
2017-01-16 08:47:33 -0800

    Add continous variable plotting

--- scripts/plotSwayArea/app.R
+++ scripts/plotSwayArea/app.R
@@ -23,26 +23,26 @@ source("read.R")
 
 data <-
   df %>% 
-  select(matches("testCondition|concussionHx|sport|gender|age|swayArea")) %>% 
+  select(matches("testCondition|concussionHx|sport|gender|age|height|mass|swayArea")) %>% 
   mutate(age = factor(trunc(age)),
          swayArea = round(swayArea, digits = 2))
 xvar <- names(data) %>% .[. != "swayArea"]
+xcont <- c("height", "mass")
+xcat <- xvar %>% .[!(xvar %in% xcont)]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
-  title = "Boxplots: Balance lab data",
+  title = "Plot: Balance lab data",
   
   plotOutput('plot'),
   
   hr(),
   
   fluidRow(
-    column(3,
-           h4("Balance lab data"),
-           selectInput('x', 'X', xvar),
-           selectInput('color', 'Color', c('None', xvar))
-    )
+    column(3, h4("Balance lab data")),
+    column(3, selectInput('x', 'X', xvar)),
+    column(3, selectInput('color', 'Color', c('None', xcat)))
   )
 )
 
@@ -54,15 +54,27 @@ server <- function(input, output) {
     G <- 
       data %>% 
       ggplot(aes_string(x = input$x)) + 
-      geom_boxplot(aes(y = swayArea), alpha = 1/4) +
       scale_y_continuous("Sway Area") +
       theme_bw()
     
-    if (input$color != 'None')
-      G <- G + 
+    if (input$x %in% xcat) {
+      G <- G + geom_boxplot(aes(y = swayArea), alpha = 1/4)
+    }
+      
+    if (input$x %in% xcont) {
+      G <- 
+        G + 
+        geom_point(aes(y = swayArea), alpha = 1/2, size = 3) +
+        geom_smooth(aes(y = swayArea), method = "lm", se = FALSE)
+    }
+      
+    if (input$color != 'None') {
+      G <- 
+        G + 
         aes_string(color = input$color, fill = input$color) +
         scale_color_brewer(palette = "Set1") +
         scale_fill_brewer(palette = "Set1")
+    }
     
     print(G)
     
